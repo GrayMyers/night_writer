5a069d6e6b81180f27622985c5a31a867eab3edb
Grayson Myers <graymyers10@gmail.com>
2020-11-08 16:37:07 -0700

    Add require for dictionary module file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require 'minitest/pride'
 require 'mocha/minitest'
 SimpleCov.start
 #Dir["./lib/*"].each{|file| require file}
-#can't do this because it will include the runner 
-require "file_reader.rb"
-require "file_writer.rb"
+#can't do this because it will include the runner
+require "./lib/file_reader.rb"
+require "./lib/file_writer.rb"
+require "./lib/dictionary.rb"

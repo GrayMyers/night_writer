a2516097688441aede4033040829456830826ec3
Grayson Myers <graymyers10@gmail.com>
2020-11-08 16:04:15 -0700

    Change test_helper to explicitly require files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'mocha/minitest'
 SimpleCov.start
-Dir["./lib/*"].each{|file| require file}
+#Dir["./lib/*"].each{|file| require file}
+#can't do this because it will include the runner 
+require "file_reader.rb"
+require "file_writer.rb"

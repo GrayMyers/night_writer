9a0de373212bb076b303a41f183dee83a6f88da4
Grayson Myers <graymyers10@gmail.com>
2020-11-08 15:26:29 -0700

    Add test for write method of FileWriter class

--- test/file_writer_test.rb
+++ test/file_writer_test.rb
@@ -1,12 +1,21 @@
 require "./test/test_helper"
 class FileWriterTest < Minitest::Test
   def setup
-    @writer = FileWriter.new("example.txt")
+    @writer = FileWriter.new("example.txt",true)
   end
 
   def test_it_exists_and_has_attributes
     assert_instance_of FileWriter, @writer
 
     assert_equal "example.txt", @writer.file_to_write
+  end
+
+  def test_it_writes
+    expected_hash = {
+      filepath: "example.txt",
+      text: ["This","is","another","message!"],
+      file_exists: false
+    }
+    assert_equal expected_hash, @writer.write(["This","is","another","message!"])
   end
 end

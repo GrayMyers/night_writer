cc941b2168a350014c617fcd2077f9ad9653c7d0
Grayson Myers <graymyers10@gmail.com>
2020-11-08 16:15:38 -0700

    Update runner to create and write to file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -4,4 +4,7 @@ require "./lib/file_writer.rb"
 reader = FileReader.new(ARGV[0])
 writer = FileWriter.new(ARGV[1])
 
+message = reader.read
+writer.write(message)
+
 puts "Created #{writer.file_to_write} containing #{writer.chars} characters"

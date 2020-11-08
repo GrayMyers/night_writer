f9d254b1aa848b814235d8209241a76ac04e9dcb
Grayson Myers <graymyers10@gmail.com>
2020-11-08 15:23:10 -0700

    Make file writer test inherit from test library

--- test/file_writer_test.rb
+++ test/file_writer_test.rb
@@ -1,12 +1,12 @@
 require "./test/test_helper"
-class FileWriter
+class FileWriterTest < Minitest::Test
   def setup
     @writer = FileWriter.new("example.txt")
   end
 
   def test_it_exists_and_has_attributes
     assert_instance_of FileWriter, @writer
-    
+
     assert_equal "example.txt", @writer.file_to_write
   end
 end

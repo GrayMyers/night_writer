b43b9e1e499852ef258daf0b15b47603d4d8fee7
Grayson Myers <graymyers10@gmail.com>
2020-11-08 16:08:33 -0700

    Add chars variable to file writer class

--- lib/file_writer.rb
+++ lib/file_writer.rb
@@ -1,18 +1,24 @@
 class FileWriter
-  attr_reader :file_to_write
+  attr_reader :file_to_write, :chars
   def initialize(filepath)
     @file_to_write = filepath
+    @chars = 0
   end
 
   def write(lines)
     remove_if_exists
     file_obj = File.new(@file_to_write, "w")
-    write_to_file(file_obj,lines)
+
+    input = concat_lines(lines)
+    write_to_file(file_obj,input)
+
+    @chars = input.length
+
     file_obj.close
   end
 
-  def write_to_file(file_obj,lines)
-    file_obj.puts(concat_lines(lines))
+  def write_to_file(file_obj,input_string)
+    file_obj.puts(input_string)
   end
 
   def remove_if_exists

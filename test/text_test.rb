e2000067f3cc7365cfd06dceccaf083fa25c24ad
Grayson Myers <graymyers10@gmail.com>
2020-11-08 17:14:11 -0700

    Fix typo

--- test/text_test.rb
+++ test/text_test.rb
@@ -11,7 +11,7 @@ class TextTest < Minitest::Test
   def test_it_exists_and_has_attributes
     assert_instance_of Text, @text
 
-    assert_equal "hello world", @text.original
+    assert_equal "hello", @text.original
     assert_equal ([@char,@char,@char,@char,@char]), @text.characters
   end
 end

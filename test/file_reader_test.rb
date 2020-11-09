743faddc89d5849d83c76a6a0c32428a6ec35fc4
Grayson Myers <graymyers10@gmail.com>
2020-11-08 19:37:08 -0700

    Stub out file reading to eliminate impurity in tests

--- test/file_reader_test.rb
+++ test/file_reader_test.rb
@@ -2,7 +2,15 @@ require "./test/test_helper"
 
 class FileReaderTest < Minitest::Test
   def setup
+    lines_arr = ["This\n","is\n","a\n","message!"]
+    
+    file_obj = mock()
+    file_obj.stubs("readlines").returns(lines_arr)
+
+    File.stubs("new").returns(file_obj)
+
     @file_reader = FileReader.new("message.txt")
+
   end
 
   def test_it_exists_and_has_attributes
@@ -12,7 +20,7 @@ class FileReaderTest < Minitest::Test
   end
 
   def test_it_can_read_file
-    expected_arr = "hello world"
+    expected_arr = "This is a message!"
     assert_equal expected_arr, @file_reader.read
   end
 end

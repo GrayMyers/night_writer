f49abe2764cad2c888c891ab6c47d52f7cbfbb18
Grayson Myers <graymyers10@gmail.com>
2020-11-08 18:00:53 -0700

    Update runner to perform braille conversion

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,10 +1,11 @@
 require "./lib/file_reader.rb"
 require "./lib/file_writer.rb"
+require "./lib/text.rb"
 
 reader = FileReader.new(ARGV[0])
 writer = FileWriter.new(ARGV[1])
 
-message = reader.read
-writer.write(message)
+text = Text.new(reader.read)
+writer.write(text.braille_text)
 
 puts "Created #{writer.file_to_write} containing #{writer.chars} characters"

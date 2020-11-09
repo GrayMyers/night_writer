f3f186f462fd3cac50f4e7e4bb328b696c626bc6
Grayson Myers <graymyers10@gmail.com>
2020-11-08 17:08:26 -0700

    Add existence and attributes tests for Text class

---
+++ test/text_test.rb
@@ -0,0 +1,17 @@
+require "./test/test_helper.rb"
+
+class TextTest < Minitest::Test
+  def setup
+    @char = mock()
+    @char.stubs("character").returns("h")
+    Character.stubs("from_english").returns(@char)
+    @text = Text.new("hello")
+  end
+
+  def test_it_exists_and_has_attributes
+    assert_instance_of Text, @text
+
+    assert_equal "hello world", @text.original
+    assert_equal ([@char,@char,@char,@char,@char],@text.characters)
+  end
+end
